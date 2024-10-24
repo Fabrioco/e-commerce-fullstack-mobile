e1f824bd208d2e520d59307bbe67652af18c9ca8
Fabricio Lopes <fabriciooliveiralopes50@gmail.com>
2024-10-23 23:44:36 -0300

    :books: doc: Paginas criadas

---
+++ src/app/Informations.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Informations() {
+  return (
+    <View className="bg-white flex-1">
+      <Text>Informação</Text>
+    </View>
+  );
+}
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,13 +3,6 @@ import { getStatusBarHeight } from "react-native-status-bar-height";
 
 const statusBarHeight = getStatusBarHeight();
 
-export default function Header({ title }: { title: string }) {
-  return (
-    <View
-      className="w-full p-4 bg-black"
-      style={{ paddingTop: statusBarHeight }}
-    >
-      <Text className="text-white text-lg font-bold">{title}</Text>
-    </View>
-  );
+export default function Header() {
+  return <View style={{ paddingTop: statusBarHeight }}></View>;
 }
