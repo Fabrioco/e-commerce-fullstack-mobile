87403bf2754b8e5ac570751352c98986573e0746
Fabricio Lopes <fabriciooliveiralopes50@gmail.com>
2024-10-23 23:43:09 -0300

    :sparkles: feat: Sistema de rotas com TabBar

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,67 @@
-import { Stack } from "expo-router";
 import "../assets/styles/global.css";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/FontAwesome5";
+
+import Home from "./Home";
+import OtherPage from "./Cart";
+import Informations from "./Informations";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }}  />;
+  const Tab = createBottomTabNavigator();
+  return (
+    <NavigationContainer independent>
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          tabBarStyle: {
+            elevation: 0,
+            shadowOpacity: 0,
+            height: 50,
+            display: "flex",
+            borderWidth: 0,
+            borderTopWidth: 0,
+            alignContent: "center",
+          },
+          tabBarItemStyle: {
+            height: 50,
+            width: 30,
+          },
+          tabBarLabel: () => null,
+          tabBarActiveTintColor: "#fff",
+          tabBarActiveBackgroundColor: "#074740",
+          tabBarInactiveTintColor: "#00000090",
+          headerShown: false,
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Icon name="home" color={color} size={25} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Cart"
+          component={OtherPage}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Icon name="shopping-bag" color={color} size={25} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Informations"
+          component={Informations}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Icon name="info" color={color} size={25} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
